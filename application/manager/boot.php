bb460628a5dce562583b33b7fc8027aed091bb1f
Mick <mickfreeze@gmail.com>
2012-05-28 22:56:13 +0200

    Proper .htaccess file yeah!

--- application/manager/boot.php
+++ application/manager/boot.php
@@ -1,7 +1,27 @@
 <?php
 
-$request_path = $_SERVER['PATH_INFO'];
-$request_query = $_SERVER['QUERY_STRING'];
+echo '<pre>', print_r($_SERVER, true), '</pre>';
+
+function request_path() {
+
+	if(isset($_SERVER['PATH_INFO'])) {
+		return $_SERVER['PATH_INFO'];
+	}
+
+	if(isset($_SERVER['REQUEST_URI'])) {
+		$request_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+		if($request_path) {
+			return rawurldecode($request_path);
+		}
+
+	}
+
+	return null;
+
+}
+
+echo "<h1>Request path: ".request_path()."</h1>";
 
 $manager_directory = dirname(__FILE__);
 $module_directory = dirname($manager_directory);
